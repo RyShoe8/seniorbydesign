4aa8830d290ae09e664b9eceeecb5f4443db58ad
RyShoe8 <30275036+RyShoe8@users.noreply.github.com>
2026-01-13 02:52:04 -0600

    Fix missing Link import in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import NewsletterCTA from '@/components/NewsletterCTA';
 import TestimonialsCarousel from '@/components/TestimonialsCarousel';
 import PortfolioCarousel from '@/components/PortfolioCarousel';
