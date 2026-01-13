fab250209f99b0b512ed72e14e933c93ca3cf48c
RyShoe8 <30275036+RyShoe8@users.noreply.github.com>
2026-01-13 03:51:19 -0600

    Add Google Analytics tracking code sitewide

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -4,6 +4,19 @@ export default function Analytics() {
   return (
     <>
       {/* Analytics Tracking Codes */}
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0DW324N5VE"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0DW324N5VE');
+        `}
+      </Script>
       {/* Ahrefs Analytics */}
       <Script
         src="https://analytics.ahrefs.com/analytics.js"
